ac862734f5aadf5d3c471a4bc3ee6f1807020b02
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 08:24:11 -0400

    Initial Commit

---
+++ sketch.js
@@ -0,0 +1,13 @@
+// declare xPos globally with a value of 25
+
+function setup() {
+  createCanvas(500, 500);
+  background(0);
+}
+
+function draw() {
+  fill(0, 255, 0);
+  // modify the ellipse function to take xPos as the x position. 
+  ellipse(0, 250, 50, 50)
+  
+}
